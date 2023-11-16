d5cbd08dbf864438093727cc0964b73347339667
jsakamoto <junichi.jp@gmail.com>
2023-11-17 02:24:55 +0900

    Tweaking to run on .NET 8

---
+++ SampleSites/Net8App/BlazorApp1/BlazorApp1.Client/Program.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddSpeechRecognition();
+
+await builder.Build().RunAsync();
---
+++ SampleSites/Net8App/BlazorApp1/BlazorApp1/Program.cs
@@ -0,0 +1,37 @@
+using BlazorApp1.Components;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSpeechRecognition();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorApp1.Client.Components.SpeechRecognitionComponent).Assembly);
+
+app.Run();
